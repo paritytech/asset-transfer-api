21fea6e4bb218b774eddab94a9e8bb54437243e8
tarikgul <tarik@parity.io>
2023-01-30 13:59:49 -0500

    cleanup the class api

--- src/AssetTransferAPI.ts
+++ src/AssetTransferAPI.ts
@@ -1,11 +1,11 @@
 import { ApiPromise } from '@polkadot/api';
+import { Bytes } from '@polkadot/types';
 
 interface ITransferArgsOpts {
 	/**
-	 * TODO: We should set this to a set amount of accepted values.
 	 * Signing Payload vs Call
 	 */
-	format?: string;
+	format?: 'payload' | 'call';
 	/**
 	 * AssetId to pay fee's on the current common good parachain.
 	 * Statemint: default DOT
@@ -31,12 +31,13 @@ export class AssetsTransferAPI {
 	}
 
 	/**
+	 * TBD
+	 *
 	 * @param chainId ID of the destination (para) chain (‘0’ for Relaychain)
 	 * @param destAddr Address of destination account
 	 * @param assetId ID of the asset to be transferred (‘0’ for DOT)
 	 * @param amount Amount of the token to transfer
 	 * @param opts Options
-	 *
 	 */
 	public createTransferTransaction(
 		chainId: string | number,
@@ -48,7 +49,12 @@ export class AssetsTransferAPI {
 		console.log(chainId, destAddr, assetId, amount, opts);
 	}
 
-	public estimateFee() {
-		console.log('estimate fee');
+	/**
+	 * Return a partialFee of the
+	 *
+	 * @param tx Transaction to estimate the fee for
+	 */
+	public estimateFee(tx: Bytes | string) {
+		console.log(tx);
 	}
 }
