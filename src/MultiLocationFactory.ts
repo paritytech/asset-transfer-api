0851bf6034b42e035cc2285edda6d7e8ede9d286
tarikgul <tarik@parity.io>
2023-02-27 13:45:45 -0500

    multilocation class

---
+++ src/MultiLocationFactory.ts
@@ -0,0 +1,16 @@
+import { ApiPromise } from "@polkadot/api";
+import { MultiLocation } from '@polkadot/types/interfaces';
+
+export class MultiLocationFactory {
+    static createBeneficiary(
+        api: ApiPromise
+    ): MultiLocation {
+        
+    }
+
+    static createDest(
+        api: ApiPromise
+    ) {
+
+    }
+}
