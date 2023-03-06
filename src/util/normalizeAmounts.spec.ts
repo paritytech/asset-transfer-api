1de0a5b62f113a14f7c5af21bd8e767c2deb4e09
tarikgul <tarik@parity.io>
2023-03-06 10:08:50 -0500

    add normalizeAmounts util

---
+++ src/util/normalizeAmounts.spec.ts
@@ -0,0 +1,21 @@
+import { normalizeAmounts } from './normalizeAmounts';
+
+describe('normalizeAccounts', () => {
+    it('Should handle an array with both strings and numbers', () => {
+        const vals = ['100', 100];
+
+        expect(normalizeAmounts(vals)).toEqual(['100', '100'])
+    });
+
+    it('Should handle just string correctly', () => {
+        const vals = ['100', '100'];
+
+        expect(normalizeAmounts(vals)).toEqual(['100', '100'])
+    });
+
+    it('Should handle just numbers correctly', () => {
+        const vals = [100, 100];
+
+        expect(normalizeAmounts(vals)).toEqual(['100', '100'])
+    });
+});
