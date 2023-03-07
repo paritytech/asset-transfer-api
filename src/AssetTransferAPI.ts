aa82082954325a582542498c6a791d3b0e774652
tarikgul <tarik@parity.io>
2023-03-07 15:28:21 -0500

    lint

--- src/AssetTransferAPI.ts
+++ src/AssetTransferAPI.ts
@@ -95,7 +95,7 @@ export class AssetsTransferAPI {
 	 * TODO: When we are actively using this change it over to `private`.
 	 * TODO: Should this be moved because we wont have the MultiLocation until we pass this
 	 * into the typecreation.
-	 * 
+	 *
 	 * Fetch the xcmVersion to use for a given chain. If the supported version doesn't for
 	 * a given destination we use the on storage safe version.
 	 *
