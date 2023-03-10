82622dfddbfce575fe4943e6732ffcb07e2441cc
CurlyBracketEffect <25679386+CurlyBracketEffect@users.noreply.github.com>
2023-03-10 14:56:39 -0500

    feat: Fetch assets pallet id (#39)
    * feat: Fetch assets pallet id
    
    * fix: ran linter

---
+++ src/createXcmTypes/util/fetchPalletInstanceId.ts
@@ -0,0 +1,18 @@
+import { ApiPromise } from '@polkadot/api';
+
+/**
+ * This fetches the metadata for the chain we are connected to and searches for the Assets pallet and returns its index.
+ *
+ * @param api ApiPromise
+ */
+export const fetchPalletInstanceId = async (
+	api: ApiPromise
+): Promise<string> => {
+	const metadata = await api.rpc.state.getMetadata();
+	const pallets = metadata.asV14.pallets;
+	const assetsPallet = pallets.filter(
+		(pallet) => pallet.name.toString() === 'Assets'
+	);
+
+	return assetsPallet[0].index.toString();
+};
