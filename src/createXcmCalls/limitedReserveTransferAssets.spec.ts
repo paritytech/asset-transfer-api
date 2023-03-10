5f3a0fe3b71ddee3fadeb051f1084fc9350e3cc1
tarikgul <tarik@parity.io>
2023-03-10 10:33:47 -0500

    lint

--- src/createXcmCalls/limitedReserveTransferAssets.spec.ts
+++ src/createXcmCalls/limitedReserveTransferAssets.spec.ts
@@ -16,7 +16,7 @@ describe('limitedReserveTransferAssets', () => {
 				'1000',
 				0
 			);
-	
+
 			expect(ext.toHex()).toBe(
 				'0xec041f0800010200f5d5714c084c112843aca74f8c498da06cc5a2d63153b825189baa51043b1f0b000101a10f00040a020532060491010000000000'
 			);
@@ -31,7 +31,7 @@ describe('limitedReserveTransferAssets', () => {
 				'1000',
 				1
 			);
-	
+
 			expect(ext.toHex()).toBe(
 				'0xfc041f080100010100f5d5714c084c112843aca74f8c498da06cc5a2d63153b825189baa51043b1f0b01010100a10f0104000002043205040091010000000000'
 			);
@@ -64,10 +64,10 @@ describe('limitedReserveTransferAssets', () => {
 					'1000',
 					1
 				);
-	
+
 			expect(err).toThrowError(
 				"Can't find the `polkadotXcm` or `xcmPallet` pallet with the given API"
 			);
 		});
-	})
+	});
 });
--- src/createXcmTypes/SystemToPara.spec.ts
+++ src/createXcmTypes/SystemToPara.spec.ts
@@ -162,22 +162,22 @@ describe('XcmVersionedMultiLocation Generation', () => {
 	});
 	describe('WeightLimit', () => {
 		// NOTE: for V0, V1, and V2 Weightlimit just uses V2 so we only need to test once.
-		// No matter the version if its equal to or less than 2, it will alwyas default to V2. 
+		// No matter the version if its equal to or less than 2, it will alwyas default to V2.
 		it('Should work when given a weightLimit', () => {
 			const weightLimit = SystemToPara.createWeightLimit(
 				mockSystemApi,
 				'100000000'
 			);
 			expect(weightLimit.toJSON()).toStrictEqual({
-				limited: 100000000
+				limited: 100000000,
 			});
 		});
 		it('Should work when no weightLimit is present', () => {
 			const weightLimit = SystemToPara.createWeightLimit(mockSystemApi);
 
 			expect(weightLimit.toJSON()).toStrictEqual({
-				unlimited: null
-			})
+				unlimited: null,
+			});
 		});
-	})
+	});
 });
