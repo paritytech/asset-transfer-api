75c5e4394643dfd635a8544fb8f11dfdcdd6685b
tarikgul <tarik@parity.io>
2023-03-13 21:29:22 -0400

    Add test for establishXcmPallet

---
+++ src/createXcmCalls/util/establishXcmPallet.spec.ts
@@ -0,0 +1,14 @@
+import { mockRelayApi } from '../../testHelpers/mockRelayApi';
+import { mockSystemApi } from '../../testHelpers/mockSystemApi';
+import { establishXcmPallet } from './establishXcmPallet';
+
+describe('establishXcmPallet', () => {
+	it('Should detect xcmPallet pallet correctly', () => {
+		const res = establishXcmPallet(mockRelayApi);
+		expect(res).toEqual('xcmPallet');
+	});
+	it('Should detect polkadotXcm pallet correctly', () => {
+		const res = establishXcmPallet(mockSystemApi);
+		expect(res).toEqual('polkadotXcm');
+	});
+});
