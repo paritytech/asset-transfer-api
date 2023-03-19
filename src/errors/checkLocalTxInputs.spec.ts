020bf1ac77d10ecf56b44cafe5a2da142d5cf7fc
tarikgul <tarik@parity.io>
2023-03-18 21:01:09 -0400

    add BaseError and checkLocalTxInputs

---
+++ src/errors/checkLocalTxInputs.spec.ts
@@ -0,0 +1,55 @@
+// Copyright 2017-2023 Parity Technologies (UK) Ltd.
+// This file is part of @substrate/asset-transfer-api.
+//
+// Substrate API Sidecar is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import { checkLocalTxInput } from './checkLocalTxInputs';
+
+describe('checkLocalTxInput', () => {
+	it('Should correctly throw the error `Invalid address format`', () => {
+		const err = () => checkLocalTxInput('0x00', ['1'], ['1']);
+		expect(err).toThrowError('Invalid address format');
+	});
+	it('Should correctly throw the error `Invalid decoded address checksum`', () => {
+		const err = () =>
+			checkLocalTxInput(
+				'5GoKvZWG5ZPYL1WUovuHW3zJBWBP5eT8CbqjdRY4Q6iMaDwU',
+				['1'],
+				['1']
+			);
+		expect(err).toThrowError('Invalid decoded address checksum');
+	});
+	it('Should correctly throw an error for incorrect length on `assetds`', () => {
+		const err = () =>
+			checkLocalTxInput(
+				'5EnxxUmEbw8DkENKiYuZ1DwQuMoB2UWEQJZZXrTsxoz7SpgG',
+				['1', '2'],
+				['1']
+			);
+		expect(err).toThrowError(
+			'Local transactions must have the `assetIds` input be a length of 1, and the `amounts` input be a length of 1'
+		);
+	});
+	it('Should correctly throw an error for incorrect length on `assetds`', () => {
+		const err = () =>
+			checkLocalTxInput(
+				'5EnxxUmEbw8DkENKiYuZ1DwQuMoB2UWEQJZZXrTsxoz7SpgG',
+				['1'],
+				['1', '2']
+			);
+		expect(err).toThrowError(
+			'Local transactions must have the `assetIds` input be a length of 1, and the `amounts` input be a length of 1'
+		);
+	});
+});
---
+++ src/errors/checkLocalTxInputs.ts
@@ -0,0 +1,46 @@
+// Copyright 2017-2023 Parity Technologies (UK) Ltd.
+// This file is part of @substrate/asset-transfer-api.
+//
+// Substrate API Sidecar is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import { validateAddress } from '../validate/validateAddress';
+import { BaseError } from './BaseError';
+
+/**
+ * Check a local transactions inputs to ensure they are correct.
+ * If there is an issue it will throw a descriptive message.
+ *
+ * @param addr
+ * @param assetIds
+ * @param amounts
+ */
+export const checkLocalTxInput = (
+	addr: string,
+	assetIds: string[],
+	amounts: string[]
+) => {
+	// TODO: Add Ethereum support
+	// Ensure the address is a valid Substrate address
+	const [isValid, errorMsg] = validateAddress(addr);
+	if (!isValid) {
+		throw new BaseError(errorMsg as string);
+	}
+
+	// Ensure the lengths in assetIds and amounts is correct
+	if (assetIds.length !== 1 || amounts.length !== 1) {
+		throw new BaseError(
+			'Local transactions must have the `assetIds` input be a length of 1, and the `amounts` input be a length of 1'
+		);
+	}
+};
