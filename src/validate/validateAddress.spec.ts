e3444ea5b4613eac6d2e594b07326ad01a22fb59
tarikgul <tarik@parity.io>
2023-03-18 20:08:08 -0400

    Add validateAddress

---
+++ src/validate/validateAddress.spec.ts
@@ -0,0 +1,25 @@
+import { validateAddress } from './validateAddress';
+
+describe('validateAddress', () => {
+	it('Should not error with a valid substrate address', () => {
+		expect(
+			validateAddress('5EnxxUmEbw8DkENKiYuZ1DwQuMoB2UWEQJZZXrTsxoz7SpgG')
+		).toEqual([true, undefined]);
+	});
+	it('Should error with a nonsense address', () => {
+		expect(validateAddress('hello')).toEqual([
+			false,
+			'Invalid base58 character "l" (0x6c) at index 2',
+		]);
+	});
+	it('Should error with an address with invalid decoded address checksum', () => {
+		expect(
+			validateAddress('5GoKvZWG5ZPYL1WUovuHW3zJBWBP5eT8CbqjdRY4Q6iMaDwU')
+		).toEqual([false, 'Invalid decoded address checksum']);
+	});
+	it('Should error with an address missing some bytes', () => {
+		expect(
+			validateAddress('y9EMHt34JJo4rWLSaxoLGdYXvjgSXEd4zHUnQgfNzwES8b')
+		).toEqual([false, 'Invalid address format']);
+	});
+});
---
+++ src/validate/validateAddress.ts
@@ -0,0 +1,51 @@
+// Copyright 2017-2023 Parity Technologies (UK) Ltd.
+// This file is part of @substrate/asset-transfer-api.
+//
+// Substrate API Sidecar is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import { isHex } from '@polkadot/util';
+import { base58Decode, checkAddressChecksum } from '@polkadot/util-crypto';
+import { defaults } from '@polkadot/util-crypto/address/defaults';
+
+/**
+ * Verify that an address is a valid substrate address.
+ *
+ * Note: this is very similar '@polkadot/util-crypto/address/checkAddress,
+ * except it does not check the ss58 prefix.
+ *
+ * @param address potential ss58 or raw address
+ */
+export const validateAddress = (
+	address: string
+): [boolean, string | undefined] => {
+	let u8Address;
+
+	if (isHex(address)) {
+		u8Address = Uint8Array.from(Buffer.from(address.slice(2), 'hex'));
+	} else {
+		try {
+			u8Address = base58Decode(address);
+		} catch (error) {
+			return [false, (error as Error).message];
+		}
+	}
+
+	if (defaults.allowedEncodedLengths.includes(u8Address.length)) {
+		const [isValid] = checkAddressChecksum(u8Address);
+
+		return [isValid, isValid ? undefined : 'Invalid decoded address checksum'];
+	}
+
+	return [false, 'Invalid address format'];
+};
