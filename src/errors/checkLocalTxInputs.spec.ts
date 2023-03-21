bbc58cd395c9bb8d5917bab79397b6da4fdedcce
tarikgul <tarik@parity.io>
2023-03-21 07:18:53 -0400

    fix naming in test

--- src/errors/checkLocalTxInputs.spec.ts
+++ src/errors/checkLocalTxInputs.spec.ts
@@ -17,14 +17,14 @@
 import { checkLocalTxInput } from './checkLocalTxInputs';
 
 describe('checkLocalTxInput', () => {
-	it('Should correctly throw an error for incorrect length on `assetds`', () => {
-		const err = () => checkLocalTxInput(['1', '2'], ['1']);
+	it('Should correctly throw an error for incorrect length on `assetIds`', () => {
+		const err = () => checkLocalTxInput(['1', '2'], ['10000']);
 		expect(err).toThrowError(
 			'Local transactions must have the `assetIds` input be a length of 1, and the `amounts` input be a length of 1'
 		);
 	});
-	it('Should correctly throw an error for incorrect length on `assetds`', () => {
-		const err = () => checkLocalTxInput(['1'], ['1', '2']);
+	it('Should correctly throw an error for incorrect length on `amounts`', () => {
+		const err = () => checkLocalTxInput(['1'], ['10000', '20000']);
 		expect(err).toThrowError(
 			'Local transactions must have the `assetIds` input be a length of 1, and the `amounts` input be a length of 1'
 		);
