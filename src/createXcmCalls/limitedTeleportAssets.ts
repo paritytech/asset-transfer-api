1bcffb13af17008abebea849f799dd3ea95ea148
Tarik Gul <47201679+TarikGul@users.noreply.github.com>
2023-03-29 16:13:40 -0400

    add teleport assets calls (#90)

---
+++ src/createXcmCalls/limitedTeleportAssets.ts
@@ -0,0 +1,47 @@
+// Copyright 2023 Parity Technologies (UK) Ltd.
+
+import type { ApiPromise } from '@polkadot/api';
+import type { SubmittableExtrinsic } from '@polkadot/api/submittable/types';
+import type { ISubmittableResult } from '@polkadot/types/types';
+
+import { createXcmTypes } from '../createXcmTypes';
+import { IDirection } from '../types';
+import { normalizeArrToStr } from '../util/normalizeArrToStr';
+import { establishXcmPallet } from './util/establishXcmPallet';
+
+/**
+ * Build a Polkadot-js SubmittableExtrinsic for a `limitedTeleportAssets` call.
+ *
+ * @param api ApiPromise
+ * @param direction Denotes the xcm direction of the call.
+ * @param destAddr The address the funds will be transfered too.
+ * @param assetIds An array of asset ids. Note, this should be the same size and order as amounts.
+ * @param amounts An array of amounts. Note, this should be the same size and order as assetIds.
+ * @param destChainId The id of the destination chain. This will be zero for a relay chain.
+ * @param xcmVersion Supported XCM version.
+ */
+export const limitedTeleportAssets = (
+	api: ApiPromise,
+	direction: IDirection,
+	destAddr: string,
+	assetIds: string[],
+	amounts: string[],
+	destChainId: string,
+	xcmVersion: number,
+	weightLimit?: string
+): SubmittableExtrinsic<'promise', ISubmittableResult> => {
+	const pallet = establishXcmPallet(api);
+	const ext = api.tx[pallet].limitedTeleportAssets;
+	const typeCreator = createXcmTypes[direction];
+	const beneficiary = typeCreator.createBeneficiary(api, destAddr, xcmVersion);
+	const dest = typeCreator.createDest(api, destChainId, xcmVersion);
+	const assets = typeCreator.createAssets(
+		api,
+		normalizeArrToStr(amounts),
+		xcmVersion,
+		assetIds
+	);
+	const weightLimitType = typeCreator.createWeightLimit(api, weightLimit);
+
+	return ext(beneficiary, dest, assets, 0, weightLimitType);
+};
