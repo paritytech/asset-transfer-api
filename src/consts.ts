88aa98670e6e845f2c6649166dfe8390c5b192d3
tarikgul <tarik@parity.io>
2023-03-30 09:28:04 -0400

    set default XCM version to 2

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 
 export const SYSTEM_PARACHAINS_NAMES = ['statemine', 'statemint', 'westmint'];
 export const SYSTEM_PARACHAINS_IDS = ['1000'];
-export const DEFAULT_XCM_VERSION = 1;
+export const DEFAULT_XCM_VERSION = 2;
