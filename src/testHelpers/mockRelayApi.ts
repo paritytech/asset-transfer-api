f9d61320959d43c8109ff3ef21fbdcb510a605e4
tarikgul <tarik@parity.io>
2023-03-30 11:45:50 -0400

    reestablish metadata to be v9400 from westend

--- src/testHelpers/mockRelayApi.ts
+++ src/testHelpers/mockRelayApi.ts
@@ -1,6 +1,6 @@
 // Copyright 2023 Parity Technologies (UK) Ltd.
 
 import { createApiWithAugmentations } from './createApiWithAugmentations';
-import { polkadotV9380 } from './metadata/polkadotV9380';
+import { westendV9380 } from './metadata/westendV9400';
 
-export const mockRelayApi = createApiWithAugmentations(polkadotV9380);
+export const mockRelayApi = createApiWithAugmentations(westendV9380);
