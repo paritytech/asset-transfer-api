2fb97dfa89032178c6f754d0f6599c0e4f9321ab
Tarik Gul <47201679+TarikGul@users.noreply.github.com>
2023-04-26 11:22:35 -0400

    Initial `checkXcmTxInputs` implementation (#122)

---
+++ src/errors/checkXcmTxInputs.ts
@@ -0,0 +1,28 @@
+import type { IDirection } from '../types';
+import { BaseError } from './BaseError';
+
+/**
+ * This will check the given inputs and ensure there is no issues when constructing
+ * the xcm transaction.
+ *
+ * TODO: This should be expanded upon and inputs may be added. This is just a base implementation.
+ *
+ * @param assetIds
+ * @param amounts
+ * @param xcmDirection
+ */
+export const checkXcmTxInputs = (
+	assetIds: string[],
+	amounts: string[],
+	xcmDirection: IDirection
+) => {
+	const isRelayDirection = xcmDirection.toLowerCase().includes('relay');
+	/**
+	 * Lengths should match, and indicies between both the amounts and assetIds should match.
+	 */
+	if (assetIds.length !== amounts.length && !isRelayDirection) {
+		throw new BaseError(
+			'`amounts`, and `assetIds` fields should match in length when constructing a tx from a parachain to a parachain or locally on a system parachain.'
+		);
+	}
+};
