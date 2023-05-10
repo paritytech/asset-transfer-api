254fa7a542c14ac38388f928bae3bda9ee679c3a
Ryan Mullen <ryanfmullen@gmail.com>
2023-05-10 13:21:29 -0700

    add documentation for AssetTransferApi and Types

--- typedoc.config.cjs
+++ typedoc.config.cjs
@@ -1,12 +1,24 @@
 module.exports = {
 	entryPoints: [
-		'./src',
+		'./src/index.ts',
 	],
 	exclude: [
 		'**/*spec.ts',
 		'node_modules/**',
 		'./src/testHelpers/**',
 	],
+	// "excludeNotDocumented": true,
+	// "navigationLinks": {
+    //     "ATApi": "./docs/classes"
+    // },
+	// "sidebarLinks": {
+    //     "ATApi": "./docs/classes"
+    // },
+	// "navigation": {
+    //     "includeCategories": true,
+    //     "includeGroups": false
+    // },
+    // "categorizeByGroup": false,
 	includeVersion: true,
 	excludeExternals: true,
 	excludePrivate: true,
