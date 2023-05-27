682923bd12a1e1234118fda18faae9054736b2c2
tarikgul <tarik@parity.io>
2023-05-27 16:00:15 -0400

    Add accountkey20 to SystemToPara and tests

--- src/createXcmTypes/SystemToPara.spec.ts
+++ src/createXcmTypes/SystemToPara.spec.ts
@@ -30,6 +30,31 @@ describe('SystemToPara XcmVersioned Generation', () => {
 
 			expect(beneficiary.toJSON()).toStrictEqual(expectedRes);
 		});
+		it('Should work for V2 for an Ethereum Address', () => {
+			const beneficiary = SystemToPara.createBeneficiary(
+				mockSystemApi,
+				'0x96Bd611EbE3Af39544104e26764F4939924F6Ece',
+				2
+			);
+
+			const expectedRes = {
+				v2: {
+					parents: 0,
+					interior: {
+						x1: {
+							accountKey20: {
+								key: '0x96bd611ebe3af39544104e26764f4939924f6ece',
+								network: {
+									any: null,
+								},
+							},
+						},
+					},
+				},
+			};
+
+			expect(beneficiary.toJSON()).toStrictEqual(expectedRes);
+		});
 		it('Should work for V3', () => {
 			const beneficiary = SystemToPara.createBeneficiary(
 				mockSystemApi,
@@ -45,6 +70,29 @@ describe('SystemToPara XcmVersioned Generation', () => {
 							accountId32: {
 								id: '0xf5d5714c084c112843aca74f8c498da06cc5a2d63153b825189baa51043b1f0b',
 								network: null,
+							},
+						},
+					},
+				},
+			};
+
+			expect(beneficiary.toJSON()).toStrictEqual(expectedRes);
+		});
+		it('Should work for V3 for an Ethereum Address', () => {
+			const beneficiary = SystemToPara.createBeneficiary(
+				mockSystemApi,
+				'0x96Bd611EbE3Af39544104e26764F4939924F6Ece',
+				3
+			);
+
+			const expectedRes = {
+				v3: {
+					parents: 0,
+					interior: {
+						x1: {
+							accountKey20: {
+								key: '0x96bd611ebe3af39544104e26764f4939924f6ece',
+								network: null
 							},
 						},
 					},
