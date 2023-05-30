efe9203336da7994f5a78705c3d04cf0c71d3f73
marshacb <cameron.marshall12@gmail.com>
2023-05-30 16:38:28 -0400

    add dedupe ts and spec

---
+++ src/createXcmTypes/util/dedupeMultiAssets.spec.ts
@@ -0,0 +1,7 @@
+// Copyright 2023 Parity Technologies (UK) Ltd.
+
+describe('dedupeMultiAssets', () => {
+    it('Should dedupe a sorted list of MultiAssets', () => {
+        const multiAssets: MultiAsset[] = []
+    })
+})
