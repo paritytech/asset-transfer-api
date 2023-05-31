d28a6757e73b6aacfcdcb64c62177fd260ff6851
marshacb <cameron.marshall12@gmail.com>
2023-05-31 10:23:50 -0400

    add tests for deduping multiassets

--- src/createXcmTypes/util/dedupeMultiAssets.spec.ts
+++ src/createXcmTypes/util/dedupeMultiAssets.spec.ts
@@ -1,7 +1,82 @@
 // Copyright 2023 Parity Technologies (UK) Ltd.
 
+import { MultiAsset } from '../../types';
+import { dedupeMultiAssets } from './dedupeMultiAssets';
+
 describe('dedupeMultiAssets', () => {
     it('Should dedupe a sorted list of MultiAssets', () => {
-        const multiAssets: MultiAsset[] = []
+        const expected: MultiAsset[] = [
+                {
+                    fun: {
+                        Fungible: '100000',
+                    },
+                    id: {
+                        Concrete: {
+                            interior: {
+                                X2: [{ PalletInstance: '50' }, { GeneralIndex: '1984' }],
+                            },
+                            parents: 0,
+                        },
+                    },
+                },
+                {
+                    fun: {
+                        Fungible: '200000',
+                    },
+                    id: {
+                        Concrete: {
+                            interior: {
+                                X2: [{ PalletInstance: '50' }, { GeneralIndex: '1984' }],
+                            },
+                            parents: 1,
+                        },
+                    },
+                },
+        ];
+        const multiAssets: MultiAsset[] = [
+            {
+                fun: {
+                    Fungible: '100000',
+                },
+                id: {
+                    Concrete: {
+                        interior: {
+                            X2: [{ PalletInstance: '50' }, { GeneralIndex: '1984' }],
+                        },
+                        parents: 0,
+                    },
+                },
+            },
+            {
+                fun: {
+                    Fungible: '100000',
+                },
+                id: {
+                    Concrete: {
+                        interior: {
+                            X2: [{ PalletInstance: '50' }, { GeneralIndex: '1984' }],
+                        },
+                        parents: 0,
+                    },
+                },
+            },
+            {
+                fun: {
+                    Fungible: '200000',
+                },
+                id: {
+                    Concrete: {
+                        interior: {
+                            X2: [{ PalletInstance: '50' }, { GeneralIndex: '1984' }],
+                        },
+                        parents: 1,
+                    },
+                },
+            },
+        ];
+
+        const deduped = dedupeMultiAssets(multiAssets);
+
+        expect(deduped.length).toEqual(expected.length);
     })
 })
---
+++ src/createXcmTypes/util/dedupeMultiAssets.ts
@@ -0,0 +1,23 @@
+import { MultiAsset } from "../../types";
+
+export const dedupeMultiAssets = (multiAssets: MultiAsset[]): MultiAsset[] => {
+    const dedupedAssets: MultiAsset[] = [];
+
+    for (let i = 0; i < multiAssets.length; i++) {
+        const multiAsset = multiAssets[i];
+        
+        if (i === 0) {
+            dedupedAssets.push(multiAsset);
+            continue;
+        }
+
+        const previousAsset = dedupedAssets[dedupedAssets.length-1];
+        if (JSON.stringify(multiAsset) === JSON.stringify(previousAsset)) {
+            continue;
+        }
+
+        dedupedAssets.push(multiAsset);
+    }
+
+    return dedupedAssets;
+}
