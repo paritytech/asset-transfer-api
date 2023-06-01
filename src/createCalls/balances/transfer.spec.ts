220062f11a4eb6028cba4fea591fd6371cbc2e13
tarikgul <tarik@parity.io>
2023-06-01 09:29:22 -0400

    Add balances calls for transfers

---
+++ src/createCalls/balances/transfer.spec.ts
@@ -0,0 +1,17 @@
+// Copyright 2023 Parity Technologies (UK) Ltd.
+
+import { mockSystemApi } from '../../testHelpers/mockSystemApi';
+import { transfer } from './transfer';
+
+describe('transfer', () => {
+	it('Should construct a valid transfer extrinsic', () => {
+		const res = transfer(
+			mockSystemApi,
+			'0xf5d5714c084c112843aca74f8c498da06cc5a2d63153b825189baa51043b1f0b',
+			'10000'
+		);
+		expect(res.toHex()).toEqual(
+			'0x98040a0000f5d5714c084c112843aca74f8c498da06cc5a2d63153b825189baa51043b1f0b419c'
+		);
+	});
+});
---
+++ src/createCalls/balances/transferKeepAlive.spec.ts
@@ -0,0 +1,17 @@
+// Copyright 2023 Parity Technologies (UK) Ltd.
+
+import { mockSystemApi } from '../../testHelpers/mockSystemApi';
+import { transferKeepAlive } from './transferKeepAlive';
+
+describe('transfer', () => {
+	it('Should construct a valid transfer extrinsic', () => {
+		const res = transferKeepAlive(
+			mockSystemApi,
+			'0xf5d5714c084c112843aca74f8c498da06cc5a2d63153b825189baa51043b1f0b',
+			'10000'
+		);
+		expect(res.toHex()).toEqual(
+			'0x98040a0300f5d5714c084c112843aca74f8c498da06cc5a2d63153b825189baa51043b1f0b419c'
+		);
+	});
+});
