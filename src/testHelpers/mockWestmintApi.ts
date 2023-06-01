2ebb83a77a2a97682e3f4d5f46e140885086b461
marshacb <cameron.marshall12@gmail.com>
2023-06-01 14:52:16 -0400

    copyright

--- src/testHelpers/mockWestmintApi.ts
+++ src/testHelpers/mockWestmintApi.ts
@@ -4,4 +4,3 @@ import { createApiWithAugmentations } from './createApiWithAugmentations';
 import { westmint9425 } from './metadata/westmintV9425';
 
 export const mockWestmintApi = createApiWithAugmentations(westmint9425);
-
