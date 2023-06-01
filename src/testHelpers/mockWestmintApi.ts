583b9a997b7b90072b0b9a0eebe6a2b55497401d
marshacb <cameron.marshall12@gmail.com>
2023-06-01 14:41:54 -0400

    add copyright for mock westmint api

--- src/testHelpers/mockWestmintApi.ts
+++ src/testHelpers/mockWestmintApi.ts
@@ -1,3 +1,5 @@
+// Copyright 2023 Parity Technologies (UK) Ltd.
+
 import { createApiWithAugmentations } from './createApiWithAugmentations';
 import { westmint9425 } from './metadata/westmintV9425';
 
