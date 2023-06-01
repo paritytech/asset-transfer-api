d3277bbe0f399ebf33388cec18566867a3478b0d
marshacb <cameron.marshall12@gmail.com>
2023-06-01 12:44:24 -0400

    update westmint metadata name to 9425

--- src/testHelpers/mockWestmintApi.ts
+++ src/testHelpers/mockWestmintApi.ts
@@ -1,4 +1,4 @@
 import { createApiWithAugmentations } from './createApiWithAugmentations';
-import { westmint9420 } from './metadata/westmintV9420';
+import { westmint9425 } from './metadata/westmintV9425';
 
-export const mockWestmintApi = createApiWithAugmentations(westmint9420);
+export const mockWestmintApi = createApiWithAugmentations(westmint9425);
