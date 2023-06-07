e7c44323ef8a6b4b09605873342af36562cf5f6c
tarikgul <tarik@parity.io>
2023-06-07 12:50:11 -0400

    inline docs

--- src/createXcmCalls/util/fetchSafeXcmVersion.ts
+++ src/createXcmCalls/util/fetchSafeXcmVersion.ts
@@ -7,19 +7,10 @@ import { DEFAULT_XCM_VERSION } from '../../consts';
 import { establishXcmPallet } from './establishXcmPallet';
 
 /**
- * TODO: When we are actively using this change it over to `private`.
- * TODO: Should this be moved because we wont have the MultiLocation until we pass this
- * into the typecreation.
- *
- * Fetch the xcmVersion to use for a given chain. If the supported version doesn't for
- * a given destination we use the on storage safe version.
- *
- * @param xcmVersion The version we want to see is supported
- * @param multiLocation Destination multilocation
- */
-/**
  * Fetch for a safe Xcm Version from the chain, if none exists the
  * in app default version will be used.
+ *
+ * @param api ApiPromise
  */
 export const fetchSafeXcmVersion = async (api: ApiPromise): Promise<u32> => {
 	const pallet = establishXcmPallet(api);
