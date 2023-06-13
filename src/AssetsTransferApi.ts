8b4bbc4c0f52841af5be13bd0efa61adbc95b6a9
Cameron Marshall <cameron.marshall12@gmail.com>
2023-06-13 19:15:14 -0400

    implement pays with fee origin (#187)
    * add paysWithFeeOrigin
    
    * move createPaysWithFeeOriginPayload into AssetsTransferApi
    
    * remove unused import
    
    * update decodedExtrinsic to return non callIndex call data for submittable, extrinsic and payload formats
    
    * update types and add comment for createPayload
    
    * add check to ensure paysWithFeeOrigin is a valid and sufficient asset
    added tests
    
    * add inline doc for ApiInfo, update docs
    
    * update inline doc
    
    * remove comment
    
    * lint
    
    * move checkIsSufficient into api
    
    * lint

--- src/AssetsTransferApi.ts
+++ src/AssetsTransferApi.ts
@@ -24,6 +24,7 @@ import {
 	teleportAssets,
 } from './createXcmCalls';
 import {
+	BaseError,
 	checkBaseInputTypes,
 	checkLocalTxInput,
 	checkXcmTxInputs,
@@ -39,8 +40,10 @@ import {
 	Format,
 	LocalTransferTypes,
 	Methods,
+	SubmittableMethodData,
 	TransferArgsOpts,
 	TxResult,
+	UnsignedTransaction,
 } from './types';
 
 /**
@@ -82,13 +85,13 @@ export class AssetsTransferApi {
 	 * @param amounts Array of the amounts of each token to transfer
 	 * @param opts Options
 	 */
-	public createTransferTransaction<T extends Format>(
+	public async createTransferTransaction<T extends Format>(
 		destChainId: string,
 		destAddr: string,
 		assetIds: string[],
 		amounts: string[],
 		opts?: TransferArgsOpts<T>
-	): TxResult<T> {
+	): Promise<TxResult<T>> {
 		/**
 		 * Ensure all the inputs are the corrects primitive and or object types.
 		 * It will throw an error if any are incorrect.
@@ -139,12 +142,14 @@ export class AssetsTransferApi {
 							: assets.transfer(_api, addr, assetIds[0], amounts[0]);
 					palletMethod = `assets::${method}`;
 				}
-				return this.constructFormat(
+
+				return await this.constructFormat(
 					tx,
 					'local',
 					null,
 					palletMethod,
-					opts?.format
+					opts?.format,
+					opts?.paysWithFeeOrigin
 				);
 			} else {
 				/**
@@ -160,7 +165,8 @@ export class AssetsTransferApi {
 					'local',
 					null,
 					palletMethod,
-					opts?.format
+					opts?.format,
+					opts?.paysWithFeeOrigin
 				);
 			}
 		}
@@ -245,7 +251,8 @@ export class AssetsTransferApi {
 			xcmDirection,
 			xcmVersion,
 			txMethod,
-			opts?.format
+			opts?.format,
+			opts?.paysWithFeeOrigin
 		);
 	}
 	/**
@@ -354,13 +361,14 @@ export class AssetsTransferApi {
 	 * @param tx A polkadot-js submittable extrinsic
 	 * @param format The format to return the tx in.
 	 */
-	private constructFormat<T extends Format>(
+	private async constructFormat<T extends Format>(
 		tx: SubmittableExtrinsic<'promise', ISubmittableResult>,
 		direction: Direction | 'local',
 		xcmVersion: number | null = null,
 		method: Methods,
-		format?: T
-	): TxResult<T> {
+		format?: T,
+		paysWithFeeOrigin?: string
+	): Promise<TxResult<T>> {
 		const { _api } = this;
 		const fmt = format ? format : 'payload';
 		const result: TxResult<T> = {
@@ -385,11 +393,10 @@ export class AssetsTransferApi {
 		}
 
 		if (fmt === 'payload') {
-			result.tx = _api.registry
-				.createType('ExtrinsicPayload', tx, {
-					version: tx.version,
-				})
-				.toHex() as ConstructedFormat<T>;
+			result.tx = (await this.createPayload(
+				tx,
+				paysWithFeeOrigin
+			)) as ConstructedFormat<T>;
 		}
 
 		return result;
@@ -445,24 +452,139 @@ export class AssetsTransferApi {
 				}
 			);
 
-			const extrinsicMethodInfo = extrinsicPayload.method.toHuman()?.toString();
+			const call = _api.registry.createType('Call', extrinsicPayload.method);
+			const decodedMethodInfo = JSON.stringify(call.toHuman());
 
-			if (extrinsicMethodInfo) {
-				return extrinsicMethodInfo;
-			}
+			return decodedMethodInfo;
 		} else if (fmt === 'call') {
 			const call = _api.registry.createType('Call', encodedTransaction);
 
-			return call.toString();
+			const decodedMethodInfo = JSON.stringify(call.toHuman());
+
+			return decodedMethodInfo;
 		} else if (fmt === 'submittable') {
 			const extrinsic = _api.registry.createType(
 				'Extrinsic',
 				encodedTransaction
 			);
 
-			return extrinsic.method.toString();
+			const decodedMethodInfo = JSON.stringify(extrinsic.method.toHuman());
+
+			return decodedMethodInfo;
 		}
 
 		return '';
 	}
+
+	/**
+	 * returns an ExtrinsicPayload
+	 *
+	 * @param tx SubmittableExtrinsic<'promise', ISubmittableResult>
+	 * @param paysWithFeeOrigin string
+	 */
+	public createPayload = async (
+		tx: SubmittableExtrinsic<'promise', ISubmittableResult>,
+		paysWithFeeOrigin?: string
+	): Promise<`0x${string}`> => {
+		let assetId = 0;
+
+		// if a paysWithFeeOrigin is provided and the chain is of system origin
+		// we assign the assetId to the value of paysWithFeeOrigin
+		const isOriginSystemParachain = SYSTEM_PARACHAINS_NAMES.includes(
+			this._specName.toLowerCase()
+		);
+
+		if (paysWithFeeOrigin && isOriginSystemParachain) {
+			assetId = Number.parseInt(paysWithFeeOrigin);
+			const isNotANumber = Number.isNaN(assetId);
+
+			if (isNotANumber) {
+				throw new BaseError(
+					`paysWithFeeOrigin value must be a valid number. Received: ${paysWithFeeOrigin}`
+				);
+			}
+
+			const isSufficient = await this.checkAssetIsSufficient(assetId);
+
+			if (!isSufficient) {
+				throw new BaseError(
+					`asset with assetId ${assetId} is not a sufficient asset to pay for fees`
+				);
+			}
+		}
+
+		const submittableString = JSON.stringify(tx.toHuman());
+		const submittableData: SubmittableMethodData = JSON.parse(
+			submittableString
+		) as unknown as SubmittableMethodData;
+		const addr = submittableData.method.args.dest.id;
+		const lastHeader = await this._api.rpc.chain.getHeader();
+		const blockNumber = this._api.registry.createType(
+			'BlockNumber',
+			lastHeader.number.toNumber()
+		);
+		const method = this._api.registry.createType('Call', tx);
+		const era = this._api.registry.createType('ExtrinsicEra', {
+			current: lastHeader.number.toNumber(),
+			period: 64,
+		});
+
+		const nonce = await this._api.rpc.system.accountNextIndex(addr);
+
+		const unsignedPayload: UnsignedTransaction = {
+			specVersion: this._api.runtimeVersion.specVersion.toHex(),
+			transactionVersion: this._api.runtimeVersion.transactionVersion.toHex(),
+			assetId,
+			address: addr,
+			blockHash: lastHeader.hash.toHex(),
+			blockNumber: blockNumber.toHex(),
+			era: era.toHex(),
+			genesisHash: this._api.genesisHash.toHex(),
+			method: method.toHex(),
+			nonce: nonce.toHex(),
+			signedExtensions: [
+				'CheckNonZeroSender',
+				'CheckSpecVersion',
+				'CheckTxVersion',
+				'CheckGenesis',
+				'CheckMortality',
+				'CheckNonce',
+				'CheckWeight',
+				'ChargeTransactionPayment',
+			],
+			tip: this._api.registry.createType('Compact<Balance>', 0).toHex(),
+			version: tx.version,
+		};
+
+		const extrinsicPayload = this._api.registry.createType(
+			'ExtrinsicPayload',
+			unsignedPayload,
+			{
+				version: unsignedPayload.version,
+			}
+		);
+
+		return extrinsicPayload.toHex();
+	};
+
+	/**
+	 * checks the chains state to determine whether an asset is valid
+	 * if it is valid, it returns whether it is marked as sufficient for paying fees
+	 *
+	 * @param assetId number
+	 * @returns Promise<boolean>
+	 */
+	public checkAssetIsSufficient = async (assetId: number): Promise<boolean> => {
+		try {
+			const asset = (await this._api.query.assets.asset(assetId)).unwrap();
+
+			if (asset.isSufficient.toString().toLowerCase() === 'true') {
+				return true;
+			}
+
+			return false;
+		} catch (err: unknown) {
+			throw new BaseError(`assetId ${assetId} does not match a valid asset`);
+		}
+	};
 }
