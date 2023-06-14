0139b168dcb92620b795af3099bd7d62b0a5aa3f
Tarik Gul <47201679+TarikGul@users.noreply.github.com>
2023-06-14 15:06:37 -0400

    Add all directional examples (#194)

---
+++ examples/relayToPara.ts
@@ -0,0 +1,41 @@
+/**
+ * When importing from @substrate/asset-transfer-api it would look like the following
+ *
+ * import { AssetsTransferApi, constructApiPromise } from '@substrate/asset-transfer-api'
+ */
+import { AssetsTransferApi } from '../src/AssetsTransferApi';
+import { constructApiPromise } from '../src/constructApiPromise';
+
+/**
+ * In this example we are creating a call to send 1 KSM from a Kusama (Relay Chain) account
+ * to a Moonriver (Parachain) account, where the `xcmVersion` is set to 2, and the `isLimited` declaring that
+ * it will be `unlimited` since there is no `weightLimit` option as well.
+ *
+ * NOTE: When `isLimited` is true it will use the `limited` version of the either `reserveAssetTransfer`, or `teleportAssets`.
+ */
+const main = async () => {
+	const { api, specName, safeXcmVersion } = await constructApiPromise(
+		'wss://kusama-rpc.polkadot.io'
+	);
+	const assetApi = new AssetsTransferApi(api, specName, safeXcmVersion);
+
+	try {
+		const callHex = await assetApi.createTransferTransaction(
+			'2023',
+			'0xF977814e90dA44bFA03b6295A0616a897441aceC',
+			['KSM'],
+			['1000000000000'],
+			{
+				format: 'call',
+				isLimited: true,
+				xcmVersion: 2,
+			}
+		);
+
+		console.log(callHex);
+	} catch (e) {
+		console.error(e);
+	}
+};
+
+main().finally(() => process.exit());
