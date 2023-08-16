71b608f5e12d6220db806b60376662635c1db0df
marshacb <cameron.marshall12@gmail.com>
2023-08-16 13:59:18 -0400

    update comment

--- src/createXcmTypes/util/isSystemChain.ts
+++ src/createXcmTypes/util/isSystemChain.ts
@@ -1,7 +1,7 @@
 // Copyright 2023 Parity Technologies (UK) Ltd.
 
 /**
- * Determines if a chain is a system chain based on the value of its chainId being strictly less than 2000
+ * Determines if a chain is a system chain based on the value of its chainId being strictly greater than 0 and less than 2000
  * @param chainId
  * @returns boolean
  */
