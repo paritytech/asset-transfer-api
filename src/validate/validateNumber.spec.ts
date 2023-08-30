31d5acb07c6bf5c97bdfbd88cdcf4c033931d94f
tarikgul <tarik@parity.io>
2023-08-30 10:01:02 -0400

    fix small nits

--- src/validate/validateNumber.spec.ts
+++ src/validate/validateNumber.spec.ts
@@ -1,7 +1,7 @@
 import { validateNumber } from './validateNumber';
 
 describe('validateNumber', () => {
-	it('Should correctly validate an number as a string with a length less than 16', () => {
+	it('Should correctly validate a number as a string with a length less than 16', () => {
 		const res = validateNumber('123456789000000');
 
 		expect(res).toEqual(true);
@@ -11,12 +11,12 @@ describe('validateNumber', () => {
 
 		expect(res).toEqual(false);
 	});
-	it('Should correctly validate an number as a string with a length greater than 16', () => {
+	it('Should correctly validate a number as a string with a length greater than 16', () => {
 		const res = validateNumber('123456789000000123456789');
 
 		expect(res).toEqual(true);
 	});
-	it('Should return false on an invalid number as a string with a length less than 16', () => {
+	it('Should return false on an invalid number as a string with a length greater than 16', () => {
 		const res = validateNumber('thisisatestthissisatestthisisatest');
 
 		expect(res).toEqual(false);
