957dba4ccae770b06a770a116b158382b3190b42
tarikgul <tariksnow37@gmail.com>
2023-10-23 16:27:12 -0400

    Add xcm base types

--- src/createXcmTypes/types.ts
+++ src/createXcmTypes/types.ts
@@ -3,6 +3,7 @@
 import type { ApiPromise } from '@polkadot/api';
 import { u32 } from '@polkadot/types';
 import type { VersionedMultiAssets, WeightLimitV2 } from '@polkadot/types/interfaces';
+import type { AnyJson } from '@polkadot/types/types';
 
 import type { Registry } from '../registry';
 import type { RequireOnlyOne, XCMDestBenificiary, XcmMultiLocation, XcmVersionedMultiAsset, XcmWeight } from '../types';
@@ -26,6 +27,89 @@ export type XcmJunction = {
 		key: string;
 	};
 	Parachain: string;
+};
+
+export type XcmV2MultiLocation = {
+	parents: number;
+	interior: XcmV2Junctions;
+};
+
+export type XcmV2Junctions = {
+	Here: '' | null;
+	X1: XcmV2Junction;
+	X2: [XcmV2Junction, XcmV2Junction];
+	X3: [XcmV2Junction, XcmV2Junction, XcmV2Junction];
+	X4: [XcmV2Junction, XcmV2Junction, XcmV2Junction, XcmV2Junction];
+	X5: [XcmV2Junction, XcmV2Junction, XcmV2Junction, XcmV2Junction, XcmV2Junction];
+	X6: [XcmV2Junction, XcmV2Junction, XcmV2Junction, XcmV2Junction, XcmV2Junction, XcmV2Junction];
+	X7: [XcmV2Junction, XcmV2Junction, XcmV2Junction, XcmV2Junction, XcmV2Junction, XcmV2Junction, XcmV2Junction];
+	X8: [
+		XcmV2Junction,
+		XcmV2Junction,
+		XcmV2Junction,
+		XcmV2Junction,
+		XcmV2Junction,
+		XcmV2Junction,
+		XcmV2Junction,
+		XcmV2Junction
+	];
+};
+
+export type XcmV2Junction = RequireOnlyOne<XcmV2JunctionBase>;
+
+export type XcmV2JunctionBase = {
+	Parachain: number;
+	AccountId32: { network?: XcmV2Network; id: string };
+	AccountIndex64: { network?: XcmV2Network; id: string };
+	AccountKey20: { network?: XcmV2Network; id: string };
+	PalletInstance: number;
+	GeneralIndex: string | number;
+	GeneralKey: string;
+	OnlyChild: AnyJson;
+	Plurality: { id: AnyJson; part: AnyJson };
+};
+
+export type XcmV2Network = string | null;
+
+export type XcmV3MultiLocation = {
+	parents: number;
+	interior: XcmV3Junctions;
+};
+
+export type XcmV3Junctions = {
+	Here: '' | null;
+	X1: XcmV3Junction;
+	X2: [XcmV3Junction, XcmV3Junction];
+	X3: [XcmV3Junction, XcmV3Junction, XcmV3Junction];
+	X4: [XcmV3Junction, XcmV3Junction, XcmV3Junction, XcmV3Junction];
+	X5: [XcmV3Junction, XcmV3Junction, XcmV3Junction, XcmV3Junction, XcmV3Junction];
+	X6: [XcmV3Junction, XcmV3Junction, XcmV3Junction, XcmV3Junction, XcmV3Junction, XcmV3Junction];
+	X7: [XcmV3Junction, XcmV3Junction, XcmV3Junction, XcmV3Junction, XcmV3Junction, XcmV3Junction, XcmV3Junction];
+	X8: [
+		XcmV3Junction,
+		XcmV3Junction,
+		XcmV3Junction,
+		XcmV3Junction,
+		XcmV3Junction,
+		XcmV3Junction,
+		XcmV3Junction,
+		XcmV3Junction
+	];
+};
+
+export type XcmV3Junction = RequireOnlyOne<XcmV3JunctionBase>;
+
+export type XcmV3JunctionBase = {
+	Parachain: number;
+	AccountId32: { network?: XcmV2Network; id: string };
+	AccountIndex64: { network?: XcmV2Network; id: string };
+	AccountKey20: { network?: XcmV2Network; id: string };
+	PalletInstance: number;
+	GeneralIndex: string | number;
+	GeneralKey: string;
+	OnlyChild: AnyJson;
+	Plurality: { id: AnyJson; part: AnyJson };
+	GlobalConsensus: string | AnyJson;
 };
 
 export interface CreateAssetsOpts {
