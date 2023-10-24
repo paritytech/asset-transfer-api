3a249fd01897e859bed5ee34d76d2c1e0d1e74d1
tarikgul <tariksnow37@gmail.com>
2023-10-24 16:20:23 -0400

    fix resolveMultiLocation

--- src/util/resolveMultiLocation.spec.ts
+++ src/util/resolveMultiLocation.spec.ts
@@ -1,26 +1,24 @@
 // Copyright 2023 Parity Technologies (UK) Ltd.
 
-import { adjustedMockSystemApi } from '../testHelpers/adjustedMockSystemApi';
 import { resolveMultiLocation } from './resolveMultiLocation';
 
 describe('resolveMultiLocation', () => {
-	it('Should return V3 for globalConsensus', () => {
-		const str = '{"parents":2,"interior":{"x1":{"globalConsensus":{"polkadot":null}}}}';
-		const res = resolveMultiLocation(adjustedMockSystemApi, str, 2);
-		expect(res.toRawType()).toEqual('{"parents":"u8","interior":"XcmV3Junctions"}');
-	});
-
 	it('Should correctly throw an error when multiLocation contains a generalKey junction and the xcmVersion is not 2', () => {
 		const str = `{"parents":1,"interior":{"x2":[{"parachain":2001},{"generalKey":"0x0001"}]}}`;
-		const err = () => resolveMultiLocation(adjustedMockSystemApi, str, 3);
+		const err = () => resolveMultiLocation(str, 3);
 
 		expect(err).toThrow('XcmVersion must be version 2 for MultiLocations that contain a GeneralKey junction.');
 	});
-
 	it('Should correctly not throw an error when xcmVersion is 3 and the multiLocation does not contain a generalKey Junction', () => {
 		const str = `{"parents":0,"interior":{"here": null}}`;
-		const err = () => resolveMultiLocation(adjustedMockSystemApi, str, 3);
+		const err = () => resolveMultiLocation(str, 3);
 
 		expect(err).not.toThrow();
+	});
+	it('Should correctly return a resolved multilocation object given a correct value', () => {
+		const str = `{"parents":1,"interior":{"x2":[{"parachain":2001},{"generalKey":"0x0001"}]}}`;
+		const exp = {"Parents":1,"Interior":{"X2":[{"Parachain":2001},{"Generalkey":"0x0001"}]}}
+
+		expect(resolveMultiLocation(str, 2)).toStrictEqual(exp);
 	});
 });
--- src/util/resolveMultiLocation.ts
+++ src/util/resolveMultiLocation.ts
@@ -1,24 +1,35 @@
 // Copyright 2023 Parity Technologies (UK) Ltd.
 
-import type { ApiPromise } from '@polkadot/api';
 import type { AnyJson } from '@polkadot/types/types';
 
+import { sanitizeKeys } from './sanitizeKeys';
 import { SUPPORTED_XCM_VERSIONS } from '../consts';
 import { BaseError, BaseErrorsEnum } from '../errors/BaseError';
-import type { UnionXcmMultiLocation } from '../types';
+import type { UnionXcmMultiLocation } from '../createXcmTypes/types';
 
+/**
+ * This ensures that the given multiLocation does not have certain junctions depending on the xcm version.
+ * 
+ * @param multiLocation 
+ * @param xcmVersion 
+ * @returns 
+ */
 export const resolveMultiLocation = (
-	api: ApiPromise,
 	multiLocation: AnyJson,
 	xcmVersion: number
 ): UnionXcmMultiLocation => {
 	const multiLocationStr = typeof multiLocation === 'string' ? multiLocation : JSON.stringify(multiLocation);
 	// Ensure we check this first since the main difference between v2, and v3 is `globalConsensus`
-	if (multiLocationStr.includes('globalConsensus') || multiLocationStr.includes('GlobalConsensus')) {
-		return api.registry.createType('XcmV3MultiLocation', JSON.parse(multiLocationStr));
+	const hasGlobalConsensus = multiLocationStr.includes('globalConsensus') || multiLocationStr.includes('GlobalConsensus');
+	if (xcmVersion < 3 && hasGlobalConsensus) {
+		throw new BaseError(
+			'XcmVersion must be version 2 for MultiLocations that contain a GlobalConsensus junction.',
+			BaseErrorsEnum.InvalidXcmVersion
+		)
 	}
 
-	if (xcmVersion != 2 && (multiLocationStr.includes('generalKey') || multiLocationStr.includes('GeneralKey'))) {
+	const hasGeneralKey = multiLocationStr.includes('generalKey') || multiLocationStr.includes('GeneralKey');
+	if (xcmVersion != 2 && hasGeneralKey) {
 		throw new BaseError(
 			'XcmVersion must be version 2 for MultiLocations that contain a GeneralKey junction.',
 			BaseErrorsEnum.InvalidXcmVersion
@@ -29,9 +40,5 @@ export const resolveMultiLocation = (
 		throw new BaseError(`Invalid XcmVersion for mulitLocation construction`, BaseErrorsEnum.InternalError);
 	}
 
-	if (xcmVersion === 2) {
-		return api.registry.createType('XcmV2MultiLocation', JSON.parse(multiLocationStr));
-	} else {
-		return api.registry.createType('XcmV3MultiLocation', JSON.parse(multiLocationStr));
-	}
+	return sanitizeKeys(JSON.parse(multiLocationStr) as UnionXcmMultiLocation);
 };
