98e70b818d083391a4d91d1ec599c7190232a859
tarikgul <tariksnow37@gmail.com>
2023-10-25 12:29:02 -0400

    fix getFeeAssetItemIndex.ts

--- src/util/getFeeAssetItemIndex.ts
+++ src/util/getFeeAssetItemIndex.ts
@@ -5,7 +5,7 @@ import { ApiPromise } from '@polkadot/api';
 import { getAssetId } from '../createXcmTypes/util/getAssetId';
 import { BaseError, BaseErrorsEnum } from '../errors';
 import { Registry } from '../registry';
-import { FungibleStrMultiAsset } from '../types';
+import { FungibleStrMultiAsset } from '../createXcmTypes/types';
 import { resolveMultiLocation } from '../util/resolveMultiLocation';
 import { validateNumber } from '../validate/validateNumber';
 
@@ -36,10 +36,11 @@ export const getFeeAssetItemIndex = async (
 
 		for (let i = 0; i < multiAssets.length; i++) {
 			const multiAsset = multiAssets[i];
+			const multiAssetInterior = multiAsset.id.Concrete.interior || multiAsset.id.Concrete['Interior'];
 
 			if (isRelayFeeAsset) {
 				// if the asset id is a relay asset, match Here interior
-				if (multiAsset.id.Concrete.interior.isHere) {
+				if (multiAsset.id.Concrete.interior.Here || multiAsset.id.Concrete.interior['here']) {
 					result = i;
 					break;
 				}
@@ -59,21 +60,22 @@ export const getFeeAssetItemIndex = async (
 					);
 					// if isForeignAssetsTransfer, compare the multiAsset interior to the the paysWithFeeDestGeneralIndex as a multilocation
 					if (isForeignAssetsTransfer) {
-						const paysWithFeeDestMultiLocation = resolveMultiLocation(api, paysWithFeeDestGeneralIndex, xcmVersion);
-						if (multiAsset.id.Concrete.interior.eq(paysWithFeeDestMultiLocation.interior)) {
+						const paysWithFeeDestMultiLocation = resolveMultiLocation(paysWithFeeDestGeneralIndex, xcmVersion);
+						const paysWithFeeDestMultiLocationInterior = paysWithFeeDestMultiLocation.interior || paysWithFeeDestMultiLocation['Interior'];
+						if (JSON.stringify(multiAssetInterior) === JSON.stringify(paysWithFeeDestMultiLocationInterior)) {
 							result = i;
 							break;
 						}
 					} else {
 						// if the current multiAsset is the relay asset we skip it since the
 						// pays with fee dest item is not the relay asset
-						if (multiAsset.id.Concrete.interior.isHere) {
+						if (multiAssetInterior.Here || multiAssetInterior['here']) {
 							continue;
 						}
 
 						if (
-							multiAsset.id.Concrete.interior.isX2 &&
-							multiAsset.id.Concrete.interior.asX2[1].asGeneralIndex.toString() === paysWithFeeDestGeneralIndex
+							multiAssetInterior.X2 &&
+							(multiAssetInterior.X2[1].GeneralIndex || multiAssetInterior.X2[1]['Generalindex']) === paysWithFeeDestGeneralIndex
 						) {
 							result = i;
 							break;
@@ -81,8 +83,8 @@ export const getFeeAssetItemIndex = async (
 					}
 				} else {
 					if (
-						multiAsset.id.Concrete.interior.isX2 &&
-						multiAsset.id.Concrete.interior.asX2[1].asGeneralIndex.toString() === paysWithFeeDest
+						multiAssetInterior.X2 &&
+						(multiAssetInterior.X2[1].GeneralIndex || multiAssetInterior.X2![1]['Generalindex']) === paysWithFeeDest
 					) {
 						result = i;
 						break;
@@ -94,9 +96,7 @@ export const getFeeAssetItemIndex = async (
 
 	if (result === -1) {
 		throw new BaseError(
-			`Invalid paysWithFeeDest value. ${paysWithFeeDest} did not match any asset in assets: ${multiAssets
-				.map((asset) => asset.id.Concrete.interior.toString())
-				.toString()}`,
+			`Invalid paysWithFeeDest value. ${paysWithFeeDest} did not match any asset in assets: ${multiAssets.map((asset) => JSON.stringify(asset.id.Concrete.interior))}`,
 			BaseErrorsEnum.InvalidInput
 		);
 	}
