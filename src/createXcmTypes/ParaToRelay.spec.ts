35a71d841f60ca235dc3e2d07821ec966d3aee5d
tarikgul <tariksnow37@gmail.com>
2023-11-07 17:50:28 -0500

    ParaToRelay template

---
+++ src/createXcmTypes/ParaToRelay.spec.ts
@@ -0,0 +1,2 @@
+// Copyright 2023 Parity Technologies (UK) Ltd.
+
---
+++ src/createXcmTypes/ParaToRelay.ts
@@ -0,0 +1,37 @@
+// Copyright 2023 Parity Technologies (UK) Ltd.
+
+import {
+	CreateWeightLimitOpts,
+	ICreateXcmType,
+	UnionXcmMultiAssets,
+	XcmDestBenificiary,
+	XcmMultiAsset,
+	XcmWeight,
+} from './types';
+
+export const ParaToRelay: ICreateXcmType = {
+    createBeneficiary: () => {
+
+    },
+    createDest: () => {
+
+    },
+    createAssets: () => {
+
+    },
+    createWeightLimit: () => {
+
+    },
+    createFeeAssetItem: () => {
+
+    },
+    createXTokensBeneficiary: () => {
+
+    },
+    createXTokensAsset: () => {
+
+    },
+    createXTokensFeeAssetItem: () => {
+        
+    }
+}
