05fcba62163db462cdedd627e025847c344f8a36
tarikgul <tariksnow37@gmail.com>
2023-11-08 11:55:34 -0500

    add most calls for ParaToRelay

--- src/createXcmTypes/ParaToRelay.spec.ts
+++ src/createXcmTypes/ParaToRelay.spec.ts
@@ -1,2 +1,51 @@
 // Copyright 2023 Parity Technologies (UK) Ltd.
 
+import { ParaToRelay } from './ParaToRelay';
+
+describe('ParaToRelay', () => {
+	describe('Beneficiary', () => {
+		it('Should work for V2', () => {
+			const beneficiary = ParaToRelay.createBeneficiary(
+				'0xf5d5714c084c112843aca74f8c498da06cc5a2d63153b825189baa51043b1f0b',
+				2
+			);
+
+			const expectedRes = {
+				V2: {
+					parents: 0,
+					interior: {
+						X1: {
+							AccountId32: {
+								id: '0xf5d5714c084c112843aca74f8c498da06cc5a2d63153b825189baa51043b1f0b',
+								network: 'Any',
+							},
+						},
+					},
+				},
+			};
+
+			expect(beneficiary).toStrictEqual(expectedRes);
+		});
+		it('Should work for V3', () => {
+			const beneficiary = ParaToRelay.createBeneficiary(
+				'0xf5d5714c084c112843aca74f8c498da06cc5a2d63153b825189baa51043b1f0b',
+				3
+			);
+
+			const expectedRes = {
+				V3: {
+					parents: 0,
+					interior: {
+						X1: {
+							AccountId32: {
+								id: '0xf5d5714c084c112843aca74f8c498da06cc5a2d63153b825189baa51043b1f0b',
+							},
+						},
+					},
+				},
+			};
+
+			expect(beneficiary).toStrictEqual(expectedRes);
+		});
+	});
+});
--- src/createXcmTypes/ParaToRelay.ts
+++ src/createXcmTypes/ParaToRelay.ts
@@ -1,37 +1,183 @@
 // Copyright 2023 Parity Technologies (UK) Ltd.
 
+import type { ApiPromise } from '@polkadot/api';
+
 import {
 	CreateWeightLimitOpts,
 	ICreateXcmType,
+	UnionXcAssetsMultiAsset,
 	UnionXcmMultiAssets,
 	XcmDestBenificiary,
+	XcmDestBenificiaryXcAssets,
 	XcmMultiAsset,
 	XcmWeight,
 } from './types';
 
 export const ParaToRelay: ICreateXcmType = {
-    createBeneficiary: () => {
+	/**
+	 * Create a XcmVersionedMultiLocation structured type for a beneficiary.
+	 *
+	 * @param accountId The accountId of the beneficiary.
+	 * @param xcmVersion The accepted xcm version.
+	 */
+	createBeneficiary: (accountId: string, xcmVersion: number): XcmDestBenificiary => {
+		if (xcmVersion === 2) {
+			return {
+				V2: {
+					parents: 0,
+					interior: {
+						X1: {
+							AccountId32: {
+								network: 'Any',
+								id: accountId,
+							},
+						},
+					},
+				},
+			};
+		}
 
-    },
-    createDest: () => {
+		return {
+			V3: {
+				parents: 0,
+				interior: {
+					X1: {
+						AccountId32: {
+							id: accountId,
+						},
+					},
+				},
+			},
+		};
+	},
+	/**
+	 * Create a XcmVersionedMultiLocation structured type for a destination.
+	 *
+	 * @param destId The destId in this case, which is the relay chain.
+	 * @param xcmVersion The accepted xcm version.
+	 */
+	createDest: (_: string, xcmVersion: number): XcmDestBenificiary => {
+		if (xcmVersion === 2) {
+			return {
+				V2: {
+					parents: 1,
+					interior: {
+						Here: null,
+					},
+				},
+			};
+		}
 
-    },
-    createAssets: () => {
+		return {
+			V3: {
+				parents: 1,
+				interior: {
+					Here: null,
+				},
+			},
+		};
+	},
+	/**
+	 * Create a VersionedMultiAsset structured type.
+	 *
+	 * @param amounts The amount for a relay native asset. The length will always be one.
+	 * @param xcmVersion The accepted xcm version.
+	 */
+	createAssets: (amounts: string[], xcmVersion: number): Promise<UnionXcmMultiAssets> => {
+		const multiAssets: XcmMultiAsset[] = [];
 
-    },
-    createWeightLimit: () => {
+		const amount = amounts[0];
+		const multiAsset = {
+			fun: {
+				Fungible: amount,
+			},
+			id: {
+				Concrete: {
+					interior: {
+						Here: '',
+					},
+					parents: 1,
+				},
+			},
+		} as XcmMultiAsset;
 
-    },
-    createFeeAssetItem: () => {
+		multiAssets.push(multiAsset);
 
-    },
-    createXTokensBeneficiary: () => {
+		if (xcmVersion === 2) {
+			return Promise.resolve({
+				V2: multiAssets,
+			});
+		} else {
+			return Promise.resolve({
+				V3: multiAssets,
+			});
+		}
+	},
+	/**
+	 * Create an Xcm WeightLimit structured type.
+	 *
+	 * @param opts Options that are used for WeightLimit.
+	 */
+	createWeightLimit: (opts: CreateWeightLimitOpts): XcmWeight => {
+		return opts.isLimited && opts.weightLimit?.refTime && opts.weightLimit?.proofSize
+			? {
+					Limited: {
+						refTime: opts.weightLimit?.refTime,
+						proofSize: opts.weightLimit?.proofSize,
+					},
+			  }
+			: { Unlimited: null };
+	},
+	/**
+	 * Return the correct feeAssetItem based on XCM direction.
+	 * In this case it will always be zero since there is no `feeAssetItem` for this direction.
+	 */
+	createFeeAssetItem: async (_: ApiPromise): Promise<number> => {
+		return await Promise.resolve(0);
+	},
+	createXTokensBeneficiary: (_: string, accountId: string, xcmVersion: number): XcmDestBenificiaryXcAssets => {
+		if (xcmVersion === 2) {
+			return {
+				V2: {
+					parents: 1,
+					interior: {
+						X1: { AccountId32: { id: accountId } },
+					},
+				},
+			};
+		}
 
-    },
-    createXTokensAsset: () => {
+		return {
+			V3: {
+				parents: 1,
+				interior: {
+					X1: { AccountId32: { id: accountId } },
+				},
+			},
+		};
+	},
+	createXTokensAsset: (amount: string, xcmVersion: number): Promise<UnionXcAssetsMultiAsset> => {
+		const multiAsset = {
+			id: {
+				Concrete: {
+					parents: 1,
+					interior: {
+						Here: null,
+					},
+				},
+			},
+			fun: {
+				Fungible: { Fungible: amount },
+			},
+		};
 
-    },
-    createXTokensFeeAssetItem: () => {
-        
-    }
-}
+		if (xcmVersion === 2) {
+			return Promise.resolve({ V2: multiAsset });
+		} else {
+			return Promise.resolve({ V3: multiAsset });
+		}
+	},
+	// createXTokensFeeAssetItem: () => {
+
+	// }
+};
