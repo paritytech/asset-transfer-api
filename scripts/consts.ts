0f9cd83e1b0e538c5a88bfb472a751c2a41d83c0
Alberto Nicolas Penayo <74352651+bee344@users.noreply.github.com>
2024-01-13 17:45:10 -0500

    Update consts.ts

--- scripts/consts.ts
+++ scripts/consts.ts
@@ -3,4 +3,3 @@
 export const KUSAMA_ASSET_HUB_WS_URL = 'ws://127.0.0.1:9911';
 export const ROCOCO_ALICE_WS_URL = 'ws://127.0.0.1:9900';
 export const TRAPPIST_WS_URL = 'ws://127.0.0.1:9921';
-export const MOONRIVER_WS_URL = 'ws://127.0.0.1:9931';
