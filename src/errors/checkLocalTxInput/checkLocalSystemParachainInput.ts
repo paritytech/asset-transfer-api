5ed9a0cf288ba8324c1c30034b8554fde650a8ca
Tarik Gul <47201679+TarikGul@users.noreply.github.com>
2024-01-29 09:32:35 -0500

    Update src/errors/checkLocalTxInput/checkLocalSystemParachainInput.ts
    Co-authored-by: Dominique <dominique@imodworks.io>

--- src/errors/checkLocalTxInput/checkLocalSystemParachainInput.ts
+++ src/errors/checkLocalTxInput/checkLocalSystemParachainInput.ts
@@ -1,6 +1,6 @@
 // Copyright 2023 Parity Technologies (UK) Ltd.
 
-import { ApiPromise } from '@polkadot/api';
+import type { ApiPromise } from '@polkadot/api';
 
 import { Registry } from '../../registry';
 import { checkLocalTxInput } from './checkLocalTxInput';
