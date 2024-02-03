b0a31f0a9ca2a669ffe6bcaa28160d2f951231b1
bee344 <alberto.penayo@parity.io>
2024-02-03 19:27:08 -0300

    commiting to share a test

--- src/errors/checkXcmTxInputs.spec.ts
+++ src/errors/checkXcmTxInputs.spec.ts
@@ -894,6 +894,7 @@ describe('checkParaAssets', () => {
 				true,
 				false,
 			);
+			console.log(registry.registry.kusama['1000'].foreignAssetsInfo)
 
 			expect(registry.cacheLookupForeignAsset('TNKR')).toEqual({
 				multiLocation: '{"Parents":"1","Interior":{"X2":[{"Parachain":"2125"},{"GeneralIndex":"0"}]}}',
