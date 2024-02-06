e305138f79ecf7cbd118869db2429c3f2726d0a1
Tarik Gul <47201679+TarikGul@users.noreply.github.com>
2024-02-05 19:15:44 -0500

    fix(internal): add deepEqual functionality for comparing two objects (#365)

---
+++ src/util/deepEqual.spec.ts
@@ -0,0 +1,14 @@
+import { deepEqual } from './deepEqual';
+
+describe('deepEqual', () => {
+	it('Should return true for 2 equal objects that are out of order', () => {
+		const a = { x: { y: 'z' }, z: ['1', '2', '3'] };
+		const b = { z: ['1', '2', '3'], x: { y: 'z' } };
+		expect(deepEqual(a, b)).toEqual(true);
+	});
+	it('Should return false with 2 unequal objects', () => {
+		const a = { x: { y: 'z' }, z: ['1', '2', '3'] };
+		const b = { w: { y: 'z' }, z: ['1', '2', '3'] };
+		expect(deepEqual(a, b)).toEqual(false);
+	});
+});
