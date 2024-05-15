2f15edc910a4daa9e09c800fde8364b0ce6ca47e
marshacb <cameron.marshall12@gmail.com>
2024-05-15 01:24:56 -0400

    update chainDestIsBridge check

--- src/createXcmTypes/util/chainDestIsBridge.ts
+++ src/createXcmTypes/util/chainDestIsBridge.ts
@@ -1,10 +1,18 @@
 // Copyright 2024 Parity Technologies (UK) Ltd.
 
+import { UnionXcmMultiLocation } from '../types';
+
 /**
  * Determines if the dest chain is a Global Consensus origin based on the value of its dest location
  * @param destLocation
  * @returns boolean
  */
 export const chainDestIsBridge = (destLocation: string): boolean => {
-	return destLocation.toLowerCase().includes('globalconsensus');
+	const location = JSON.parse(destLocation) as UnionXcmMultiLocation;
+
+	if (location.interior.X1 && JSON.stringify(location.interior.X1).toLowerCase().includes('globalconsensus')) {
+		return true;
+	}
+
+	return false;
 };
