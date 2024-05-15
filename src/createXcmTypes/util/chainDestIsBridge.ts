658211524e1ae33d5626c3d0ed3aebbb5b4ad762
marshacb <cameron.marshall12@gmail.com>
2024-05-15 01:34:44 -0400

    add interior check to chainDestIsBridge

--- src/createXcmTypes/util/chainDestIsBridge.ts
+++ src/createXcmTypes/util/chainDestIsBridge.ts
@@ -10,7 +10,11 @@ import { UnionXcmMultiLocation } from '../types';
 export const chainDestIsBridge = (destLocation: string): boolean => {
 	const location = JSON.parse(destLocation) as UnionXcmMultiLocation;
 
-	if (location.interior.X1 && JSON.stringify(location.interior.X1).toLowerCase().includes('globalconsensus')) {
+	if (
+		location.interior &&
+		location.interior.X1 &&
+		JSON.stringify(location.interior.X1).toLowerCase().includes('globalconsensus')
+	) {
 		return true;
 	}
 
