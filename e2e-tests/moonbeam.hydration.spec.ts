0b974d31f3b9bbd85351125b1953c8e525b95137
Cameron Marshall <cameron.marshall12@gmail.com>
2024-12-21 18:00:56 -0500

    test: update xcm v4 e2e tests (#489)

--- e2e-tests/moonbeam.hydration.spec.ts
+++ e2e-tests/moonbeam.hydration.spec.ts
@@ -141,100 +141,104 @@ describe('Moonbeam <> Hydration', () => {
 		}, 100000);
 	});
 
-	// describe('XCM V4', () => {
-	// 	const xcmVersion = 4;
+	describe('XCM V4', () => {
+		const xcmVersion = 4;
 
-	// 	test('Transfer GLMR from Moonbeam to Hydration', async () => {
-	// 		await moonbeam.dev.setStorage({
-	// 			System: {
-	// 				Account: [
-	// 					[[alith.address], { providers: 1, data: { free: '100000000000000000000000' } }], // GLMR
-	// 				],
-	// 			},
-	// 		});
+		test('Transfer GLMR from Moonbeam to Hydration', async () => {
+			await moonbeam.dev.setStorage({
+				System: {
+					Account: [
+						[[alith.address], { providers: 1, data: { free: '100000000000000000000000' } }], // GLMR
+					],
+				},
+			});
 
-	// 		const recipientInitialGLMRBalance = await hydration.api.query.tokens.accounts(hydrationRecipientAddress, 16);
+			const recipientInitialGLMRBalance = await hydration.api.query.tokens.accounts(hydrationRecipientAddress, 16);
 
-	// 		await check(recipientInitialGLMRBalance).toMatchSnapshot('hydration recipients initial glmr balance');
-	// 		expect((recipientInitialGLMRBalance as AccountData).free.toNumber()).toEqual(0);
+			await check(recipientInitialGLMRBalance).toMatchSnapshot('hydration recipients initial glmr balance');
+			expect((recipientInitialGLMRBalance as AccountData).free.toNumber()).toEqual(0);
 
-	// 		const assetTransferApi = new AssetTransferApi(moonbeam.api, 'moonbeam', xcmVersion);
-	// 		const tx = await assetTransferApi.createTransferTransaction(
-	// 			'2034',
-	// 			hydrationRecipientAddress,
-	// 			['GLMR'],
-	// 			['10000000000000000000'],
-	// 			{
-	// 				format: 'payload',
-	// 				xcmVersion,
-	// 				sendersAddr: alith.address,
-	// 			},
-	// 		);
+			const assetTransferApi = new AssetTransferApi(moonbeam.api, 'moonbeam', xcmVersion);
+			const tx = await assetTransferApi.createTransferTransaction(
+				'2034',
+				hydrationRecipientAddress,
+				['GLMR'],
+				['10000000000000000000'],
+				{
+					format: 'payload',
+					xcmVersion,
+					sendersAddr: alith.address,
+				},
+			);
 
-	// 		const extrinsic = assetTransferApi.api.registry.createType('Extrinsic', { method: tx.tx.method }, { version: 4 });
+			const extrinsic = assetTransferApi.api.registry.createType('Extrinsic', { method: tx.tx.method }, { version: 4 });
 
-	// 		await moonbeam.api.tx(extrinsic).signAndSend(alith);
-	// 		await moonbeam.dev.newBlock();
+			await moonbeam.api.tx(extrinsic).signAndSend(alith);
+			await moonbeam.dev.newBlock();
 
-	// 		await hydration.dev.newBlock();
+			await hydration.dev.newBlock();
 
-	// 		const recipientUpdatedGLMRBalance = await hydration.api.query.tokens.accounts(hydrationRecipientAddress, 16);
+			const recipientUpdatedGLMRBalance = await hydration.api.query.tokens.accounts(hydrationRecipientAddress, 16);
 
-	// 		await check(recipientUpdatedGLMRBalance).toMatchSnapshot('hydration recipients updated glmr balance');
-	// 		expect((recipientUpdatedGLMRBalance as AccountData).free.toBigInt()).toBeGreaterThan(
-	// 			(recipientInitialGLMRBalance as AccountData).free.toBigInt(),
-	// 		);
-	// 	}, 100000);
+			await check(recipientUpdatedGLMRBalance)
+				.redact({ number: 3 })
+				.toMatchSnapshot('hydration recipients updated glmr balance');
+			expect((recipientUpdatedGLMRBalance as AccountData).free.toBigInt()).toBeGreaterThan(
+				(recipientInitialGLMRBalance as AccountData).free.toBigInt(),
+			);
+		}, 100000);
 
-	// 	test('Transfer HDX from Hydration to Moonbeam', async () => {
-	// 		await hydration.dev.setStorage({
-	// 			System: {
-	// 				Account: [
-	// 					[[alice.address], { providers: 1, data: { free: '50000000000000000000000000' } }], // HDX
-	// 				],
-	// 			},
-	// 			Tokens: {
-	// 				Accounts: [
-	// 					[[alice.address, 0], { free: '50000000000000000000000000' }], // HDX
-	// 				],
-	// 			},
-	// 		});
+		test('Transfer HDX from Hydration to Moonbeam', async () => {
+			await hydration.dev.setStorage({
+				System: {
+					Account: [
+						[[alice.address], { providers: 1, data: { free: '50000000000000000000000000' } }], // HDX
+					],
+				},
+				Tokens: {
+					Accounts: [
+						[[alice.address, 0], { free: '50000000000000000000000000' }], // HDX
+					],
+				},
+			});
 
-	// 		const recipientInitialHDXBalance = (
-	// 			await moonbeam.api.query.assets.account(hdxMoonbeamAssetID, alith.address)
-	// 		).unwrapOrDefault();
+			const recipientInitialHDXBalance = (
+				await moonbeam.api.query.assets.account(hdxMoonbeamAssetID, alith.address)
+			).unwrapOrDefault();
 
-	// 		await check(recipientInitialHDXBalance).toMatchSnapshot('moonbeam recipients initial hdx balance');
-	// 		expect(recipientInitialHDXBalance.balance.toNumber()).toEqual(0);
+			await check(recipientInitialHDXBalance).toMatchSnapshot('moonbeam recipients initial hdx balance');
+			expect(recipientInitialHDXBalance.balance.toNumber()).toEqual(0);
 
-	// 		const assetTransferApi = new AssetTransferApi(hydration.api, 'hydradx', xcmVersion);
-	// 		const tx = await assetTransferApi.createTransferTransaction(
-	// 			'2004',
-	// 			alith.address,
-	// 			['HDX'],
-	// 			['10000000000000000'],
-	// 			{
-	// 				format: 'payload',
-	// 				xcmVersion,
-	// 				sendersAddr: alice.address,
-	// 			},
-	// 		);
+			const assetTransferApi = new AssetTransferApi(hydration.api, 'hydradx', xcmVersion);
+			const tx = await assetTransferApi.createTransferTransaction(
+				'2004',
+				alith.address,
+				['HDX'],
+				['10000000000000000'],
+				{
+					format: 'payload',
+					xcmVersion,
+					sendersAddr: alice.address,
+				},
+			);
 
-	// 		const extrinsic = assetTransferApi.api.registry.createType('Extrinsic', { method: tx.tx.method }, { version: 4 });
+			const extrinsic = assetTransferApi.api.registry.createType('Extrinsic', { method: tx.tx.method }, { version: 4 });
 
-	// 		await hydration.api.tx(extrinsic).signAndSend(alice);
-	// 		await hydration.dev.newBlock();
+			await hydration.api.tx(extrinsic).signAndSend(alice);
+			await hydration.dev.newBlock();
 
-	// 		await moonbeam.dev.newBlock();
+			await moonbeam.dev.newBlock();
 
-	// 		const recipientUpdatedHDXBalance = (
-	// 			await moonbeam.api.query.assets.account(hdxMoonbeamAssetID, alith.address)
-	// 		).unwrapOrDefault();
+			const recipientUpdatedHDXBalance = (
+				await moonbeam.api.query.assets.account(hdxMoonbeamAssetID, alith.address)
+			).unwrapOrDefault();
 
-	// 		await check(recipientUpdatedHDXBalance).toMatchSnapshot('moonbeam recipients updated hdx balance');
-	// 		expect(recipientUpdatedHDXBalance.balance.toBigInt()).toBeGreaterThan(
-	// 			recipientInitialHDXBalance.balance.toBigInt(),
-	// 		);
-	// 	}, 100000);
-	// });
+			await check(recipientUpdatedHDXBalance)
+				.redact({ number: 3 })
+				.toMatchSnapshot('moonbeam recipients updated hdx balance');
+			expect(recipientUpdatedHDXBalance.balance.toBigInt()).toBeGreaterThan(
+				recipientInitialHDXBalance.balance.toBigInt(),
+			);
+		}, 100000);
+	});
 });
