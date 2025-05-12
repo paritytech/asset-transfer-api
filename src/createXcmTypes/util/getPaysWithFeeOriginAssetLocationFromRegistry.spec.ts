d895ccce8548e5d15d692f225019c6e91c41daeb
Imod7 <dominique@imodworks.io>
2025-05-12 17:49:27 +0200

    updated test response

--- src/createXcmTypes/util/getPaysWithFeeOriginAssetLocationFromRegistry.spec.ts
+++ src/createXcmTypes/util/getPaysWithFeeOriginAssetLocationFromRegistry.spec.ts
@@ -99,7 +99,7 @@ describe('getPaysWithFeeOriginAssetLocationFromRegistry', () => {
 		const err = () => getPaysWithFeeOriginAssetLocationFromRegistry(assetHubAPI, paysWithFeeOriginAssetId);
 
 		expect(err).toThrow(
-			`Multiple assets found with symbol USDT:\nassetId: 66 symbol: USDT\nassetId: 67 symbol: USDT\nassetId: 2000 symbol: USDT\nassetId: 8888 symbol: USDT\nassetId: 55555 symbol: USDT\nassetId: 4000000012 symbol: USDT\nPlease provide an integer assetId or valid asset location for paysWithFeeOrigin rather than the token symbol`,
+			`Multiple assets found with symbol USDT:\nassetId: 66 symbol: USDT\nassetId: 67 symbol: USDT\nassetId: 2000 symbol: USDT\nassetId: 8888 symbol: USDT\nassetId: 55555 symbol: USDT\nassetId: 50000091 symbol: USDt\nassetId: 50000123 symbol: USDT\nassetId: 4000000012 symbol: USDT\nPlease provide an integer assetId or valid asset location for paysWithFeeOrigin rather than the token symbol`,
 		);
 	});
 });
