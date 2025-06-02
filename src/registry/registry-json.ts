a6164ae73cd1dc142e98678f80d43deb76b7b51f
andrew-ifrita <andrew.ahlers@parity.io>
2025-06-02 23:43:34 +0200

    fix: replace `assert` with `with` to support newer node versions (#577)

--- src/registry/registry-json.ts
+++ src/registry/registry-json.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 // https://github.com/isaacs/tshy?tab=readme-ov-file#commonjs-dialect-polyfills
 // @ts-ignore suppress "Import assertions are not allowed on statements that compile to CommonJS 'require' calls."
-import registry from '@substrate/asset-transfer-api-registry' assert { type: 'json' };
+import registry from '@substrate/asset-transfer-api-registry' with { type: 'json' };
 
 export { registry };
