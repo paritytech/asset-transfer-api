62640ea2560c8f7170304f9ae2220a54da433314
Andrew Ahlers <andrew.ahlers@parity.io>
2025-06-05 20:01:49 +0200

    feat: Add skippable exceptions to the example testing.

--- examples/run_examples.sh
+++ examples/run_examples.sh
@@ -3,10 +3,42 @@
 # Run all TS files and examples/ and output the failures.
 # This utility script is meant to simplify CI checks on the examples.
 
+# cd to root of project
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "$DIR/.." || exit
+
+# Associative array of files to skip. Please provide reason
+declare -A skippable=(
+	# Utility scripts
+	["examples/colors.ts"]=1
+	# Expects local endpoint to be available
+	["examples/submittable.ts"]=1
+	# TODO: Fix the following later or remove them entirely
+	# PalletNotFound: Pallet xTokens not found in the current runtime.
+	["examples/paraToParaTransferMultiAsset.ts"]=1
+	["examples/claimAssets.ts"]=1
+	["examples/paraToSystemTransferMultiAssets.ts"]=1
+	["examples/paraToSystemTransferMultiAsset.ts"]=1
+	["examples/paraToParaTransferMultiAssetWithFee.ts"]=1
+	["examples/paraToRelayTransferMultiAsset.ts"]=1
+	["examples/paraToSystemTransferMultiAssetWithFee.ts"]=1
+	# getaddrinfo ENOTFOUND moonriver-rpc.dwellir.com
+	["examples/paraToParaTransferMultiAssets.ts"]=1
+	["examples/paraToSystemParachainPrimaryNative.ts"]=1
+	# getaddrinfo ENOTFOUND paseo-asset-hub-rpc.polkadot.io
+	["examples/paseoAssetHubToRelay.ts"]=1
+	# getaddrinfo ENOTFOUND bifrost-polkadot-rpc.dwellir.com
+	["examples/fetchFeeInfo.ts"]=1
+)
+
 failed=$(mktemp)
 error=$(mktemp)
 
 for file in $(find examples/ -type f -name "*.ts"); do
+	if [[ -n "${skippable[$file]}" ]]; then
+		echo "Skipping $file. Explicitly listed exception."
+		continue
+	fi
 	echo "Running $file ..."
 	if ! npx tsx "$file" 2> "$error" > /dev/null; then
 		echo "$file" >> "$failed"
