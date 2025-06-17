3bcd76b711ca614020a306cff73c6cad946a709c
Andrew Ahlers <andrew.ahlers@parity.io>
2025-06-17 14:09:15 +0200

    feat: Add XCM V5 support to createXcmOnDestination

--- src/createXcmTypes/util/createXcmOnDestination.ts
+++ src/createXcmTypes/util/createXcmOnDestination.ts
@@ -2,12 +2,14 @@
 
 import type { AnyJson } from '@polkadot/types/types';
 
+import { DEFAULT_XCM_VERSION } from '../../consts.js';
+import { BaseError, BaseErrorsEnum } from '../../errors/BaseError.js';
 import { UnionXcmMultiLocation } from '../types.js';
 
 export const createXcmOnDestination = (
 	assets: string[],
 	beneficiary: UnionXcmMultiLocation,
-	xcmVersion: number,
+	xcmVersion: number = DEFAULT_XCM_VERSION,
 	customXcmOnDest?: string,
 ): AnyJson => {
 	const xcmMessage: AnyJson = customXcmOnDest
@@ -25,14 +27,15 @@ export const createXcmOnDestination = (
 				},
 			];
 
-	const defaultDestXcm: AnyJson =
-		xcmVersion === 3
-			? {
-					V3: xcmMessage,
-				}
-			: {
-					V4: xcmMessage,
-				};
-
-	return defaultDestXcm;
+	switch (xcmVersion) {
+		// xcm V2 was explicitly not supported previously
+		case 3:
+			return { V3: xcmMessage };
+		case 4:
+			return { V4: xcmMessage };
+		case 5:
+			return { V5: xcmMessage };
+		default:
+			throw new BaseError(`XCM version ${xcmVersion} not supported.`, BaseErrorsEnum.InvalidXcmVersion);
+	}
 };
